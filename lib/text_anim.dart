2831b1504147ac21b822d586ac8ad381fc11c035
oddlyspaced <oddlyspaced1@gmail.com>
2020-11-04 01:45:25 +0530

    text_anim: Setup for personal usage

--- lib/text_anim.dart
+++ lib/text_anim.dart
@@ -8,11 +8,6 @@ class RotateAnimatedTextKit extends StatefulWidget {
   /// Gives [TextStyle] to the text strings.
   final TextStyle textStyle;
 
-  /// Define the [Duration] of the pause between texts
-  ///
-  /// By default it is set to 500 milliseconds.
-  final Duration pause;
-
   /// Override the [Duration] of the animation by setting the duration parameter.
   ///
   /// This will set the total duration for the animated widget.
@@ -24,9 +19,6 @@ class RotateAnimatedTextKit extends StatefulWidget {
   ///
   /// By default it is set to [TextStyle.fontSize] * 10 / 3.
   final double transitionHeight;
-
-  /// Adds the onTap [VoidCallback] to the animated widget.
-  final VoidCallback onTap;
 
   /// Adds the onFinished [VoidCallback] to the animated widget.
   ///
@@ -48,52 +40,20 @@ class RotateAnimatedTextKit extends StatefulWidget {
   /// By default it is set to [TextAlign.start]
   final TextAlign textAlign;
 
-  /// Sets the number of times animation should repeat
-  ///
-  /// By default it is set to 3
-  final int totalRepeatCount;
-
-  /// Sets if the animation should repeat forever. [isRepeatingAnimation] also
-  /// needs to be set to true if you want to repeat forever.
-  ///
-  /// By default it is set to false, if set to true, [totalRepeatCount] is ignored.
-  final bool repeatForever;
-
-  /// Set if the animation should not repeat by changing the value of it to false.
-  ///
-  /// By default it is set to true.
-  final bool isRepeatingAnimation;
-
-  /// Should the animation ends up early and display full text if you tap on it ?
-  ///
-  /// By default it is set to false.
-  final bool displayFullTextOnTap;
-
   const RotateAnimatedTextKit({
     Key key,
     @required this.text,
     this.textStyle,
     this.transitionHeight,
-    this.pause = const Duration(milliseconds: 500),
     this.onFinished,
-    this.totalRepeatCount = 3,
     this.duration = const Duration(milliseconds: 2000),
-    this.onTap,
     this.alignment = Alignment.center,
     this.textDirection = TextDirection.ltr,
     this.textAlign = TextAlign.start,
-    this.displayFullTextOnTap = false,
-    this.repeatForever = false,
-    this.isRepeatingAnimation = true,
   })  : assert(null != text),
-        assert(null != pause),
-        assert(null != totalRepeatCount),
         assert(null != duration),
         assert(null != alignment),
         assert(null != textAlign),
-        assert(null != displayFullTextOnTap),
-        assert(null != repeatForever),
-        assert(null != isRepeatingAnimation),
         super(key: key);
 
   @override
@@ -111,10 +71,6 @@ class _RotatingTextState extends State<RotateAnimatedTextKit>
 
   bool _isCurrentlyPausing = false;
 
-  Timer _timer;
-
-  int _currentRepeatCount;
-
   @override
   void initState() {
     super.initState();
@@ -122,15 +78,12 @@ class _RotatingTextState extends State<RotateAnimatedTextKit>
     _transitionHeight =
         widget.transitionHeight ?? (widget.textStyle.fontSize * 10 / 3);
 
-    _currentRepeatCount = 0;
-
     _initAnimation();
     _nextAnimation();
   }
 
   @override
   void dispose() {
-    _timer?.cancel();
     _controller?.stop();
     _controller?.dispose();
     super.dispose();
@@ -145,25 +98,23 @@ class _RotatingTextState extends State<RotateAnimatedTextKit>
     );
     return GestureDetector(
       behavior: HitTestBehavior.opaque,
-      onTap: _onTap,
       child: SizedBox(
         height: _transitionHeight,
         child: _isCurrentlyPausing || !_controller.isAnimating
             ? textWidget
             : AnimatedBuilder(
-          animation: _controller,
-          child: textWidget,
-          builder: (BuildContext context, Widget child) {
-            return AlignTransition(
-              alignment: _slideIn,
-              child: Opacity(
-                opacity:
-                _fadeIn.value,
-                child: child,
+                animation: _controller,
+                child: textWidget,
+                builder: (BuildContext context, Widget child) {
+                  return AlignTransition(
+                    alignment: _slideIn,
+                    child: Opacity(
+                      opacity: _fadeIn.value,
+                      child: child,
+                    ),
+                  );
+                },
               ),
-            );
-          },
-        ),
       ),
     );
   }
@@ -192,72 +143,11 @@ class _RotatingTextState extends State<RotateAnimatedTextKit>
         curve: const Interval(0.0, 0.4, curve: Curves.easeOut),
       ),
     );
-
-    _slideOut = AlignmentTween(
-      begin: Alignment.centerLeft.add(widget.alignment).resolve(direction),
-      end: Alignment.bottomLeft.add(widget.alignment).resolve(direction),
-    ).animate(
-      CurvedAnimation(
-        parent: _controller,
-        curve: const Interval(0.7, 1.0, curve: Curves.linear),
-      ),
-    );
-
-    _fadeOut = Tween<double>(begin: 1.0, end: 0.0).animate(
-      CurvedAnimation(
-        parent: _controller,
-        curve: const Interval(0.7, 1.0, curve: Curves.easeIn),
-      ),
-    )..addStatusListener(_animationEndCallback);
   }
 
   void _nextAnimation() {
     _isCurrentlyPausing = false;
-
-    if (widget.isRepeatingAnimation &&
-        (widget.repeatForever ||
-            _currentRepeatCount != (widget.totalRepeatCount - 1))) {
-      if (!widget.repeatForever) {
-        _currentRepeatCount++;
-      }
-    } else {
-      widget.onFinished?.call();
-      return;
-    }
-
     if (mounted) setState(() {});
-
     _controller.forward(from: 0.0);
-  }
-
-  void _setPause() {
-
-    _isCurrentlyPausing = true;
-    if (mounted) setState(() {});
-  }
-
-  void _animationEndCallback(state) {
-    if (state == AnimationStatus.completed) {
-      assert(null == _timer || !_timer.isActive);
-      _timer = Timer(widget.pause, _nextAnimation);
-    }
-  }
-
-  void _onTap() {
-    if (widget.displayFullTextOnTap) {
-      if (_isCurrentlyPausing) {
-        _timer?.cancel();
-        _nextAnimation();
-      } else {
-        _controller?.stop();
-
-        _setPause();
-
-        assert(null == _timer || !_timer.isActive);
-        _timer = Timer(widget.pause, _nextAnimation);
-      }
-    }
-
-    widget.onTap?.call();
   }
 }
